a44b8cbf40fc598c0dba54267998f432ef713d8e
Charly <102165285+Charlyy95@users.noreply.github.com>
2024-05-15 15:19:49 +0200

    v0.c

--- v0.c
+++ v0.c
@@ -7,12 +7,13 @@ int main(){
 	// ouvrir le fichier 
 	FILE* f = fopen(nom, "r");
 	if(f==NULL){
+		printf ("fichier non-trouvé");
 		exit(1);
 	}
 	// recuperer une ligne
 	signed char texte [256];
 	while (fgets (texte, 255, f)!=NULL){
-		printf ("%s\n, texte);
+		printf ("%s\n", texte);
 	}
 
 	fclose (f);
